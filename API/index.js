366a29d75291cb12b85fdc276117a136ad3accd5
LamourMathys <mathys.lamoour@gmail.com>
2026-09-14 15:41:05 +0200

    feat(swaggerUI): swagger documentation

--- API/index.js
+++ API/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 const app = express()
 
 app.use(express.json())
 
 const youtubersRoutes = require('./routes/youtubersRoutes')
 app.use('/YTAPI', youtubersRoutes)
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(express.static('public'))
 app.listen(3000, () => {
